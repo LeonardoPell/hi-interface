edaa4661a18e98f776a5968b2b6e93593269c523
LeonardoPell <leonardojbpellegrino@gmail.com>
2023-06-05 14:35:07 -0300

    fix: ajusta função permissao

--- src/app/core/services/permissao.service.ts
+++ src/app/core/services/permissao.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from "@angular/core";
 import jwt_decode from 'jwt-decode';
+import { DadosUsuarioService } from "./dados-usuario.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PermissaoService {
 
-    constructor() {}
+    constructor(private _dadosUsuarioService: DadosUsuarioService) {}
 
-    validaPermissaoRotina(permissaoNecessaria: number[],permissaoAtual: number){
+    validaPermissaoRotina(permissaoNecessaria: number[]){
+        const permissaoAtual = Number(this._dadosUsuarioService.pegaDadosUsuario().nivel_obreiro);
         if(permissaoAtual === 1){
             return true;
         }
